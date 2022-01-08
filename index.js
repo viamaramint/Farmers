35f0c1f57768d73448b76d030971ca08d40541f1
viamaramint <96976163+viamaramint@users.noreply.github.com>
2022-01-08 20:14:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function openNav1() {
+  document.getElementById("mycontact").style.width = "100%";
+}
+
+function closeNav1() {
+  document.getElementById("mycontact").style.width = "0%";
+}
